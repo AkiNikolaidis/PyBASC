4356a2ba8d63766afd574991242cc2b49ac86f60
Aki Nikolaidis <g.aki.nikolaidis@gmail.com>
2017-08-08 10:54:35 -0400

    added cross-run stability comparison in RunFile

--- RunFile.py
+++ RunFile.py
@@ -8,11 +8,12 @@
 #
 #import BASC
 #from BASC import *
-import numpy as np
-from os.path import expanduser
-from basc_workflow_runner import run_basc_workflow
 import utils
 import os
+import numpy as np
+import scipy.stats
+from os.path import expanduser
+from basc_workflow_runner import run_basc_workflow
 
 home = expanduser("~")
 proc_mem= [3,6]
@@ -40,15 +41,23 @@ cross_cluster=True
 roi2_mask_file= home + '/git_repo/BASC/masks/RC_Quarter_Res.nii.gz'
 affinity_threshold= [0.5] * len(subject_file_list)
 ism_gsm_stability=[]
+ind_clust_stab_mat=[]
+ind_clust_stab_summary=[[1, 2, 3, 4, 5]]
 run=True
 
 for n_clusters in n_clusters_list:
     for output_size in output_sizes:
-        out_dir= home + '/BASC_outputs/multi_set9/dim_' + str(output_size) + '_' + str(n_clusters) + '_clusters'
+        out_dir= home + '/BASC_outputs/multi_set10/dim_' + str(output_size) + '_' + str(n_clusters) + '_clusters'
         basc_test= run_basc_workflow(subject_file_list, roi_mask_file, dataset_bootstraps, timeseries_bootstraps, n_clusters, output_size, bootstrap_list, proc_mem, cross_cluster=cross_cluster, roi2_mask_file=roi2_mask_file, affinity_threshold=affinity_threshold, out_dir=out_dir, run=run)
         ism_gsm_stability.append(np.load(out_dir + '/workflow_output/ism_gsm_corr_file/ism_gsm_corr.npy'))
-
+        ind_clust_stab_mat = np.load(out_dir + '/workflow_output/ind_group_cluster_stability_set/ind_group_cluster_stability_set.npy')
+        ind_clust_stab_summary=np.concatenate((ind_clust_stab_summary, np.array([[n_clusters, output_size, ind_clust_stab_mat.mean(), scipy.stats.variation(ind_clust_stab_mat).mean(), (ind_clust_stab_mat.mean() - scipy.stats.variation(ind_clust_stab_mat).mean())]])))
+        
     print('saving files: ism_gsm_stability')
     ism_gsm_stability_file=os.path.join(out_dir, 'ism_gsm_stability_'+ str(n_clusters)+ '.npy')
     np.save(ism_gsm_stability_file, ism_gsm_stability)
     ism_gsm_stability=[]
+
+print('saving files: ind_clust_stab_summary')
+ind_clust_stab_summary_file=os.path.join(out_dir, 'ind_clust_stab_summary.npy')
+np.save(ind_clust_stab_summary_file, ind_clust_stab_summary)
