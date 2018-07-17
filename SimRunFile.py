285651dfe125da7cb464641e7e58890652bdab87
Aki Nikolaidis <g.aki.nikolaidis@gmail.com>
2018-07-17 19:36:41 -0400

    Updating master

--- SimRunFile.py
+++ SimRunFile.py
@@ -46,9 +46,9 @@ subject_file_list = ['/users/aki.nikolaidis/git_repo/PyBASC/SimData4/sub_0corr_0
 roi_mask_file='/users/aki.nikolaidis/git_repo/PyBASC/masks/Full_BG_Sim_3mm.nii.gz'
 roi2_mask_file='/users/aki.nikolaidis/git_repo/PyBASC/masks/Yeo7_3mmMasks/Yeo_2_3mm.nii.gz'
 
-reruns=np.linspace(1,1,1)
+reruns=np.linspace(1,10,10)
 
-dataset_bootstraps_list=[1]#,10,30,100]
+dataset_bootstrap_list=[1]#,10,30,100]
 timeseries_bootstrap_list=[1]#,10,30,100]
 similarity_metric_list=['correlation'] #['correlation','euclidean','cityblock', 'cosine']
 blocklength_list=[1]#[0.5,1,2]
@@ -68,37 +68,35 @@ for rerun in reruns:
     randseed=np.random.randint(0,10000)
     np.random.seed(randseed)
 
-    for dataset_bootstraps in dataset_bootstraps_list:
+    for (dataset_bootstraps, timeseries_bootstraps) in zip(dataset_bootstrap_list,timeseries_bootstrap_list):
         bootstrap_list= list(np.ones(dataset_bootstraps, dtype=int)*dataset_bootstraps)
-    
-    
-        for timeseries_bootstraps in timeseries_bootstrap_list:
-            for similarity_metric in similarity_metric_list:
-                for blocklength in blocklength_list:
-                        for n_clusters in n_clusters_list:
-                            for output_size in output_sizes:
+        
+        for similarity_metric in similarity_metric_list:
+            for blocklength in blocklength_list:
+                for n_clusters in n_clusters_list:
+                    for output_size in output_sizes:
                                 #import pdb; pdb.set_trace()
                                 #out_dir= '/users/aki.nikolaidis/PyBASC_outputs/SimTesting/dim_' + str(output_size) + '_' + str(similarity_metric) + '_' + str(n_clusters) + '_clusters_' +str(timeseries_bootstraps) +'_IndBS_' + str(blocklength) + '_block' + similarity_metric
-                                out_dir= '/users/aki.nikolaidis/PyBASC_outputs/NewSpatialConstraintTest' + str(int(rerun)) + '_' + str(dataset_bootstraps) + 'GS' +'/' + str(n_clusters) + '_clusters'
+                        out_dir= '/users/aki.nikolaidis/PyBASC_outputs/RerunTest3_' + str(int(rerun)) + '_' + str(dataset_bootstraps) + 'GS' +'/' + str(n_clusters) + '_clusters'
         
-                                PyBASC_test=run_basc_workflow(subject_file_list, roi_mask_file, dataset_bootstraps, timeseries_bootstraps, n_clusters, output_size, bootstrap_list, proc_mem, similarity_metric, cross_cluster=cross_cluster, roi2_mask_file=roi2_mask_file, blocklength=blocklength, affinity_threshold=affinity_threshold, out_dir=out_dir, run=run)
+                        PyBASC_test=run_basc_workflow(subject_file_list, roi_mask_file, dataset_bootstraps, timeseries_bootstraps, n_clusters, output_size, bootstrap_list, proc_mem, similarity_metric, cross_cluster=cross_cluster, roi2_mask_file=roi2_mask_file, blocklength=blocklength, affinity_threshold=affinity_threshold, out_dir=out_dir, run=run)
         #                        del PyBASC_test
         #                        gc.collect()
                                 #import pdb; pdb.set_trace()
-                                ism_gsm_stability.append(np.load(out_dir + '/workflow_output/ism_gsm_corr_file/ism_gsm_corr.npy'))
-                                ind_clust_stab_mat = np.load(out_dir + '/workflow_output/ind_group_cluster_stability_set/ind_group_cluster_stability_set.npy')
-                                ind_clust_stab_summary=np.concatenate((ind_clust_stab_summary, np.array([[n_clusters, output_size, ind_clust_stab_mat.mean(), scipy.stats.variation(ind_clust_stab_mat).mean(), (ind_clust_stab_mat.mean() - scipy.stats.variation(ind_clust_stab_mat).mean())]])))
+                        ism_gsm_stability.append(np.load(out_dir + '/workflow_output/ism_gsm_corr_file/ism_gsm_corr.npy'))
+                        ind_clust_stab_mat = np.load(out_dir + '/workflow_output/ind_group_cluster_stability_set/ind_group_cluster_stability_set.npy')
+                        ind_clust_stab_summary=np.concatenate((ind_clust_stab_summary, np.array([[n_clusters, output_size, ind_clust_stab_mat.mean(), scipy.stats.variation(ind_clust_stab_mat).mean(), (ind_clust_stab_mat.mean() - scipy.stats.variation(ind_clust_stab_mat).mean())]])))
                                 
                                 #Run Group ClusterMaps
-                                gsm_file = out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/group_stability_matrix.npy'
-                                clusters_G_file = out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/clusters_G.npy'
-                                os.chdir(out_dir +'/workflow_output/basc_workflow_runner/basc/join_group_stability/')
-                                create_group_cluster_maps(gsm_file,clusters_G_file,roi_mask_file)
-                                #Run IGCM on all individual subjects
-                                clustvoxscoredir=out_dir + '/workflow_output/basc_workflow_runner/basc/individual_group_clustered_maps/mapflow/'
-                                clusters_G_file= out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/clusters_G.npy'
-                                #clusters_G = np.load(clusters_G_file)
-                                
+                        gsm_file = out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/group_stability_matrix.npy'
+                        clusters_G_file = out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/clusters_G.npy'
+                        os.chdir(out_dir +'/workflow_output/basc_workflow_runner/basc/join_group_stability/')
+                        create_group_cluster_maps(gsm_file,clusters_G_file,roi_mask_file)
+                        #Run IGCM on all individual subjects
+                        clustvoxscoredir=out_dir + '/workflow_output/basc_workflow_runner/basc/individual_group_clustered_maps/mapflow/'
+                        clusters_G_file= out_dir + '/workflow_output/basc_workflow_runner/basc/join_group_stability/clusters_G.npy'
+                        #clusters_G = np.load(clusters_G_file)
+                        
                                 #ism_nifti(roi_mask_file, n_clusters, out_dir)
                                 #gsm_nifti(roi_mask_file, n_clusters, out_dir)
                                 
@@ -116,19 +114,19 @@ for rerun in reruns:
                     #            save_igcm_nifti(clustvoxscorefile,clusters_G_file, roi_mask_file)
                     #        
                             
-                            print('saving files: ism_gsm_stability')
-                            ism_gsm_stability_file=os.path.join(out_dir, 'ism_gsm_stability_'+ str(n_clusters)+ '.npy')
-                            np.save(ism_gsm_stability_file, ism_gsm_stability)
-                            ism_gsm_stability=[]
-                    
-                        print('saving files: ind_clust_stab_summary')
-                        ind_clust_stab_summary_file=os.path.join(out_dir, 'ind_clust_stab_summary.npy')
-                        np.save(ind_clust_stab_summary_file, ind_clust_stab_summary)
+                    print('saving files: ism_gsm_stability')
+                    ism_gsm_stability_file=os.path.join(out_dir, 'ism_gsm_stability_'+ str(n_clusters)+ '.npy')
+                    np.save(ism_gsm_stability_file, ism_gsm_stability)
+                    ism_gsm_stability=[]
+            
+                print('saving files: ind_clust_stab_summary')
+                ind_clust_stab_summary_file=os.path.join(out_dir, 'ind_clust_stab_summary.npy')
+                np.save(ind_clust_stab_summary_file, ind_clust_stab_summary)
             
         #PSEUDO CODE- FOR THE ANALYSIS WITH THE HIGHEST MEAN AND LOWEST CV
         # RUN POST ANALYSIS- INCLUDING FOR LOOPING OVER ALL SUBJECTS FOLDERS AND CREATING NIFTI FILES FOR EVERY CLUSTER
         
         
-os.system('say "Hello, your analysis has completed. Please take a look at the output files, have a nice day, and dont forget to tip your server"')    
+#os.system('say "Hello, your analysis has completed. Please take a look at the output files, have a nice day, and dont forget to tip your server"')    
         
 #clustervoxscores=np.load(outdir,)
