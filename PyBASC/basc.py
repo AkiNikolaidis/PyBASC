7a175c25070c7e8711fd3425aa6ec87cec3298c4
anibalsolon <anibalsolon@gmail.com>
2018-09-18 15:46:59 -0400

    set default

--- PyBASC/basc.py
+++ PyBASC/basc.py
@@ -100,7 +100,7 @@ def nifti_individual_stability(
     n_bootstraps, n_clusters, compression_dim, similarity_metric,
     compressor=None, cross_cluster=False, cxc_compressor=None,
     cxc_roi_mask_file=None, blocklength=1, cbb_block_size=None,
-    affinity_threshold=0.0, cluster_method
+    affinity_threshold=0.0, cluster_method='ward'
 ):
     # TODO @AKI update docs
     """
@@ -229,7 +229,7 @@ def nifti_individual_stability(
 
 def map_group_stability(
     subject_stability_list, n_clusters, bootstrap_list,
-    roi_mask_file, group_dim_reduce, cluster_method
+    roi_mask_file, group_dim_reduce, cluster_method='ward'
 ):
     # TODO @AKI review doc
     """
